7769afed833222fd4d3a3e18615aace0ad89291a
Swathi Mantha <swathi.mantha@gmail.com>
2018-01-16 13:58:12 -0800

    Added BS and regex: Read webpage: requests,Beautiful Soup,regex libs

--- decode_a_web_page.py
+++ decode_a_web_page.py
@@ -15,19 +15,34 @@ import re
 import requests
 from bs4 import BeautifulSoup
 
+# some requests code here for getting r_html 
 url = 'https://www.nytimes.com/'
 r = requests.get(url)
 r_html = r.text
 
-# some requests code here for getting r_html 
-soup = BeautifulSoup(r_html,"lxml")
-heading = soup.find_all(class_="story-heading")
-#print (heading)
-i = 1
 
-for link in heading:
+soup = BeautifulSoup(r_html,"lxml")
+headings = soup.find_all(class_="story-heading")
+#print (heading)
+
+# Extracting the link using Beautiful Soup
+i = 1
+print ("********** Using Beautiful Soup**********\n\n\n")
+for heading in headings:
+    if heading.a:
+        print ("BSTitle #{0} : {1}".format(i,heading.a.text.replace("\n", " ").strip()))
+#        print (heading.a.text.replace("\n", " ").strip())
+    else:
+        print ("Title #{0} : {1}".format(i,heading.contents[0].strip()))
+#        print(heading.contents[0].strip())
+    i += 1
+
+# Extracting the link using Regex
+i = 1
+print ("\n\n\n********** Using Regex **********\n\n\n")
+for heading in headings:
     regex = r"<a.*>(\s*.*)<\/a"
-    match = re.search(regex,str(link))    
+    match = re.search(regex,str(heading))    
     if match: 
-        print ("Title #{0} : {1}".format(i,match.group(1).strip()))
+        print ("RegTitle #{0} : {1}".format(i,match.group(1).strip()))
         i += 1
