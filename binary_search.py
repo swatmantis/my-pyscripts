a1f3ec70e4396d4f48650dc74926580dc67e389d
Swathi Mantha <swathi.mantha@gmail.com>
2018-02-06 06:51:48 -0800

    binary search program

---
+++ binary_search.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+Created on Mon Feb  5 08:04:43 2018
+
+@author: Swathi
+"""
+'''
+Write a function that takes an ordered list of numbers (a list where the elements are in order from smallest to largest)
+ and another number. The function decides whether or not the given number is inside the list and returns (then prints) 
+ an appropriate boolean.
+
+Extras:
+
+    Use binary search.
+'''
+
+def find_By_BinarySearch(num,search_List):
+    'Searching num in search_List using Binary Search algorithm'
+    foundNum = "False"
+
+    if len(search_List)%2 == 0:
+        mid = int(len(search_List)/2)
+    else:
+         mid = int((len(search_List)-1)/2)
+#    print ("mid = {0}; search_List = {1}".format(mid,search_List))
+    
+    'Starting the search'
+    while len(search_List) >= 1:
+#        print ("search_List = {}".format(search_List))
+#        print ("mid = {}".format(mid))
+#        print ("search_List[mid] = {}".format(search_List[mid]))
+        if int(search_List[mid]) == num:
+#            print("mid = num")
+            foundNum = "True"
+            return foundNum
+        
+        elif int(search_List[mid]) < num:
+#            print("mid < num")
+            search_List = search_List[mid+1:]
+            return find_By_BinarySearch(num,search_List)
+        
+        elif int(search_List[mid]) > num:
+#            print("mid > num")
+            search_List = search_List[:mid]
+            return find_By_BinarySearch(num,search_List)
+
+
+if __name__=="__main__":
+    oList = input("Enter an array of numbers (comma separated): ").split(',')
+    'Converting oList into an Ordered List'
+    oList.sort() 
+    'Input a search number'
+    num = int(input("Enter a number to search: "))
+    
+    if find_By_BinarySearch(num,oList):
+        print ("\nNumber {} is in the list {}\n".format(num,oList)) 
+    else:
+        print ("\nNumber {} is NOT in the list {}\n".format(num,oList))
