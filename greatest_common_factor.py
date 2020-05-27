a7c82535cff37f4b21048bb8249911510ba9a82c
Swathi <swathi.mantha@gmail.com>
2020-05-26 23:35:30 -0700

    working code

--- greatest_common_factor.py
+++ greatest_common_factor.py
@@ -2,42 +2,23 @@
 """Find Greatest Common Factor"""
 import random
 
-def generate_random_numbers(start, stop, count):
-    random_numbers = []
-    for number in range(1, count + 1):
-        # start = 2
-        # stop = 1000
-        random_numbers.append(random.randint(start, stop))
-    return random_numbers
+def get_random_num():
+    start = 2
+    stop = 50
+    return random.randint(start, stop)
 
-def calculate_factors(number):
-    factors = []
-    for divisor in range(1, number + 1):
-        if number % divisor == 0:
-            quotient = int(number / divisor)
-            factors.append(divisor)
-    factors.sort()
-    return factors
-
-def calculate_GCF(first, second):
-    #Finding the factors of a the given numbers
-    first_factors = set(calculate_factors(first))
-    second_factors = set(calculate_factors(second))
-
-    print(f"Factors of {first}: {first_factors}")
-    print(f"Factors of {second}: {second_factors}")
-
-    gcf = max(first_factors & second_factors)
-    return gcf
+def gcd(x, y):
+    while y != 0:
+        (x, y) = (y, x % y)
+    return x
 
 def main():
     #Generating two random numbers
-    # first, second = 10, 20
-    first, second = generate_random_numbers(2, 50, 2)
+    m = get_random_num()
+    n = get_random_num()
 
     #Find and print Greatest Common Factor
-    gcf = calculate_GCF(first, second)
-    print(f"Greatest Common Factor of {first} and {second} is: {gcf}")
+    print (f"gcd of {m} and {n} is {gcd(m, n)}")
 
 if __name__ == '__main__':
     main()
